89b6336f47627ed0510e1add61d871cd48761214
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-10-16 21:31:29 +0900

    fix: refacture pages components

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,12 +1,12 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import {NextPage} from "next";
 
 const PropertyList: NextPage = () => {
   return (
-    <>
-      <Container>PROPERTY LIST</Container>
-    </>
+   <div style={{ margin: "20px 0" }}>
+    <Stack className={"container"}>PROPERTY LIST</Stack>
+   </div>
   );
 };
 
