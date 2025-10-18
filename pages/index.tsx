df8556c71eaa76469a3d81bbfb05149fe82931f3
Dostonbek Ahmadaliev <ahmadalievd384@gmail.com>
2025-10-18 16:24:56 +0900

    feat: property list publishing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import Advertisement from "@/libs/components/homepage/Advertisement";
